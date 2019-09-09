3ba380c8db69249f943c3b67de0351a15590cab2
geniusbg <geniuss0ft@yahoo.com>
2019-09-09 17:16:56 +0300

    More problems

--- advanced syntax and operation/Problem 16. Points Counter.php
+++ advanced syntax and operation/Problem 16. Points Counter.php
@@ -3,7 +3,6 @@
 $prohibited = array("@", "%", "&", "$", "*");
 
 $input = explode("|", str_replace($prohibited, "", readline()));
-// $input = explode("|", str_replace($prohibited, "", "L%@A|Odom|67"));
 
 $result = [];
 $teamScore = array();
@@ -14,22 +13,22 @@ while ($input[0] != "Result") {
     if (ctype_upper($input[0])) {
         $player = $input[1];
         $club = $input[0];
-        $temp[$input[0]][$input[1]] = intval($input[2]);
+        $temp[$club][$player] = intval($input[2]);
     } else {
         $player = $input[0];
         $club = $input[1];
-        $temp[$input[1]][$input[0]] = intval($input[2]);
+        $temp[$club][$player] = intval($input[2]);
     }
-    if (!isset($teamInfo[$club][$player]) || $temp[$club][$player] > $teamInfo[$club][$player]) {
+    if (!isset($teamInfo[$club][$player]) || $temp[$club][$player] > $teamInfo[$club][$player] || $temp[$club][$player] != 0) {
         $teamInfo[$club][$player] = $input[2];
     }
 
     $input = explode("|", str_replace($prohibited, "", readline()));
 }
 
-// $input = explode("|", str_replace($prohibited, "", "L%@A|Odom|3"));
 foreach ($teamInfo as $key => $value) {
-    // arsort($value);
+    asort($value);
+    array_multisort($teamInfo[$key], SORT_DESC);
     if (!array_key_exists($key, $teamScore)) {
         $teamScore[$key] = 0;
     }
